a25b33f7c04feea1fe524c7b951d9e5ab8cb8329
evanbowman <ebowman@bu.edu>
2017-03-25 00:13:05 -0400

    added submit button and border around input fields

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+
+function onSubmitPressed() {
+    var h3 = document.createElement("H3");
+    h3.innerHTML = "Hey there!";
+    var inputs = document.getElementById("inputs");
+    inputs.appendChild(h3);
+}
+
+window.onload = function() {
+    
+}
