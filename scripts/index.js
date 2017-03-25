dd346d7f499e8a1685f447823bd48610aab951fa
evanbowman <ebowman@bu.edu>
2017-03-25 00:43:41 -0400

    partial backend integration

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,20 @@
+const OptronServerLoc = "https://optron.herokuapp.com/job";
+
+var g_http = null;
 
 function onSubmitPressed() {
-    var h3 = document.createElement("H3");
-    h3.innerHTML = "Hey there!";
-    var inputs = document.getElementById("inputs");
-    inputs.appendChild(h3);
-}
-
-window.onload = function() {
-    
+    document.getElementById("submit-button").disabled = true;
+    g_http = new XMLHttpRequest();
+    g_http.open("POST", OptronServerLoc, true);
+    g_http.setRequestHeader("Content-type", "application/json");
+    g_http.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+            alert(g_http.responseText);
+	}
+	document.getElementById("submit-button").disabled = false;
+    }
+    g_http.send(JSON.stringify({
+	name: document.getElementById("cello-username").value,
+	password: document.getElementById("cello-password").value
+    }));
 }
